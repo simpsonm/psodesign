2ae459905a1861eaa577ea6ff0fa0e116024aaf1
Matthew W. Simpson <msimpson@lewis3.rnet.missouri.edu>
2016-08-10 14:48:22 -0500

    fixed shell file for running things on the server

--- code/psosims/psoruns.sh
+++ code/psosims/psoruns.sh
@@ -2,11 +2,11 @@
 
 #SBATCH -A stsn
 #SBATCH -J psoruns
-#SBATCH -o psoruns.Rout
+#SBATCH -o psoruns.o
 #SBATCH -e psoruns.e
 #SBATCH --mem 10000
 #SBATCH -t 10-00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=themattsimpson@gmail.com
 
-R --no-restore --no-save psoruns.R
+R --no-restore --no-save CMD BATCH psoruns.R
