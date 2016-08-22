77e7c4019c90126ac84b5719783c893defd71694
simpsonm <themattsimpson@gmail.com>
2016-08-22 00:37:44 -0500

    shell file for accpsoruns

---
+++ code/pop/accpsoruns.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#SBATCH -A stsn
+#SBATCH -J accpsorunspop
+#SBATCH -o accpsoruns.o
+#SBATCH -e accpsoruns.e
+#SBATCH --mem 5000
+#SBATCH -t 5-00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=themattsimpson@gmail.com
+
+R --no-restore --no-save CMD BATCH accpsoruns.R
