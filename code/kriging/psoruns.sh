1749ee118aa84b30548a3c8801e263c7a87a1411
simpsonm <themattsimpson@gmail.com>
2016-09-13 17:04:33 -0500

    added slurm file for kriging runs

---
+++ code/kriging/psoruns.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#SBATCH -A stsn
+#SBATCH -J psorunskriging
+#SBATCH -o psoruns.o
+#SBATCH -e psoruns.e
+#SBATCH --mem 5000
+#SBATCH -t 5-00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=themattsimpson@gmail.com
+
+R --no-restore --no-save CMD BATCH psoruns.R
