494eba36552a0368d899bad152d2ceb37fb7fd5b
Matt <themattsimpson@gmail.com>
2016-10-27 16:08:33 -0500

    wrote up pure PSO sims

--- code/psosims/psoout.R
+++ code/psosims/psoout.R
@@ -2,8 +2,11 @@ library(plyr)
 library(dplyr)
 library(reshape2)
 library(xtable)
+library(ggplot2)
+library(gridExtra)
 
 psoout <- read.csv("psosimsout.csv")
+psoout <- subset(psoout, rep <= 40)
 
 convpercent <- function(x, cutoff){
   return(mean(abs(x)<=cutoff))
@@ -43,13 +46,14 @@ save(psosum, file = "psosum.Rdata")
 
 namefun <- function(x){
   if(x$style == "AT1"){
-    style <- "AT3"
+    style <- "AT3-"
   } else if(x$style == "AT2"){
-    style <- "AT5"
+    style <- "AT5-"
+  } else if(x$style == "CI"){
+    style <- ""
   } else {
-    style <- x$style
+    style <- paste(x$style, "-", sep="")
   }
-  style <- paste(style, "-", sep="")
   if(x$type == "BBPSO"){
     parset <- switch(x$parset, "", "xp")
   } else {
@@ -74,15 +78,15 @@ namesfun <- function(x){
 }
 
 psosumsmol <- ddply(psosum, .(obj, algid, nbhd, mean, sd, con1, k1), namesfun)[,c(1,8,9,4,5,6,7)]
-colnames(psosumsmol) <- c("Obj", "Algorithm", "Nbhd", "Mean", "SD", "P", "K")
+colnames(psosumsmol) <- c("Obj", "Algorithm", "Nbhd", "Mean", "SD", "$\\widehat{P}$", "$\\widehat{K}$")
 
 
-algorder <- c("DI-PSO1", "DI-PSO2", "DI-PSO1-CF", "DI-PSO2-CF",
-              "CI-PSO1", "CI-PSO2", "CI-PSO1-CF", "CI-PSO2-CF",
+algorder <- c("PSO1", "PSO2", "PSO1-CF", "PSO2-CF",
+              "DI-PSO1", "DI-PSO2", "DI-PSO1-CF", "DI-PSO2-CF",
               "AT3-PSO1", "AT3-PSO2", "AT3-PSO1-CF", "AT3-PSO2-CF",
               "AT5-PSO1", "AT5-PSO2", "AT5-PSO1-CF", "AT5-PSO2-CF",
-              "AT3-BBPSO", "AT3-BBPSO-CF", "AT3-BBPSOxp", "AT3-BBPSOxp-CF",
-              "AT5-BBPSO", "AT5-BBPSO-CF", "AT5-BBPSOxp",   "AT5-BBPSOxp-CF")
+              "AT3-BBPSO", "AT3-BBPSOxp", "AT3-BBPSO-CF", "AT3-BBPSOxp-CF",
+              "AT5-BBPSO", "AT5-BBPSOxp", "AT5-BBPSO-CF", "AT5-BBPSOxp-CF")
 nbhdorder <- c("Global", "SS3", "SS1")
 
 psosumsmol$Algorithm <- factor(psosumsmol$Algorithm, levels = algorder)
@@ -91,174 +95,60 @@ psosumsmol$Nbhd <- factor(psosumsmol$Nbhd, levels = nbhdorder)
 
 psosumorder <- arrange(psosumsmol, Obj, Algorithm, Nbhd)
 
-## need to structure the table
-
-k <- 1
-ofk <- subset(psosumorder, Obj == k)[,-1]
-ofk1 <- subset(ofk, Nbhd == "Global")[,-2]
-ofk2 <- subset(ofk, Nbhd == "SS3")[,-2]
-ofk3 <- subset(ofk, Nbhd == "SS1")[,-2]
-ofkout <- cbind(ofk1, ofk2[,-1], ofk3[,-1])
-
-print(xtable(ofkout), include.rownames=FALSE, sanitize.text.function=identity)
-
+## this prints the tables, however it's missing the first header.
+## paste the following line (without the ##) just after the \begin{tabular} line
+## make sure to change OF1 to OF2, OF3, etc., in the those tables
+## \multicolumn{1}{l}{\begin{tabular}{lr} OF1; & Nbhd: \end{tabular}} & \multicolumn{4}{c}{Global} & \multicolumn{4}{c}{SS3} & \multicolumn{4}{c}{SS1} \\
 for(k in 1:6){
-tabout <- cbind(rbind(subset(sumlast, subset=(obj == k & Nbhd == "global" & Algorithm %in% c("PSO", "BBPSO-MC", "BBPSOxp-MC"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "global" &
-                              Algorithm %in% paste("AT-BBPSO-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "global" &
-                              Algorithm %in% paste("AT-BBPSOxp-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "global"))[35 + 1:13,])[,c(2,4,5,6,7)],
-rbind(subset(sumlast, subset=(obj == k & Nbhd == "ring-3" & Algorithm %in% c("PSO", "BBPSO-MC", "BBPSOxp-MC"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "ring-3" &
-                              Algorithm %in% paste("AT-BBPSO-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "ring-3" &
-                              Algorithm %in% paste("AT-BBPSOxp-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "ring-3"))[35 + 1:13,])[,c(4,5,6,7)],
-rbind(subset(sumlast, subset=(obj == k & Nbhd == "ring-1" & Algorithm %in% c("PSO", "BBPSO-MC", "BBPSOxp-MC"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "ring-1" &
-                              Algorithm %in% paste("AT-BBPSO-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "ring-1" &
-                              Algorithm %in% paste("AT-BBPSOxp-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"))),
-      subset(sumlast, subset=(obj == k & Nbhd == "ring-1"))[35 + 1:13,])[,c(4,5,6,7)])
-tabout$Algorithm <- mapvalues(tabout$Algorithm, from = paste("AT-BBPSO-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"),
-          to = paste("$df = ", c(rep(paste(1, ",\\enspace", sep=""), 4), rep(paste(3, ",\\enspace", sep=""), 4), rep(paste(5, ",\\enspace", sep=""), 4), rep("\\infty,", 4)), "$ $R^* =", rep(c(0.1, 0.3, 0.5, 0.7), 4), "$", sep = ""))
-tabout$Algorithm <- mapvalues(tabout$Algorithm, from = paste("AT-BBPSOxp-MC", c(rep(1, 4), rep(3, 4), rep(5, 4), rep("Inf", 4)), rep(c(0.1, 0.3, 0.5, 0.7), 4), sep = "-"),
-                              to = paste("$df = ", c(rep(paste(1, ",\\enspace", sep=""), 4), rep(paste(3, ",\\enspace", sep=""), 4), rep(paste(5, ",\\enspace", sep=""), 4), rep("\\infty,", 4)), "$ $R^* =", rep(c(0.1, 0.3, 0.5, 0.7), 4), "$", sep = ""))
-tabout$Algorithm <- mapvalues(tabout$Algorithm,
-                              from = paste("DI-PSO", c(rep(50, 3), rep(100, 3), rep(200, 3)), rep(c(1, 2, 4), 3), sep = "-"),
-                              to = paste("$\\alpha = ", c(rep(paste(50, ",\\enspace", sep=""), 3), rep("100,", 3), rep("200,", 3)), "$ $\\beta =", rep(c(1, 2, 4), 3), "$", sep = ""))
-tabout$Algorithm <- mapvalues(tabout$Algorithm,
-                              from = paste("AT-PSO", c(0.1, 0.3, 0.5, 0.7), sep="-"),
-                              to = paste("$R^* = ", c(0.1, 0.3, 0.5, 0.7), "$", sep=""))
-tabout$Algorithm <- mapvalues(tabout$Algorithm,
-                              from = c("PSO", "BBPSO-MC", "BBPSOxp-MC"),
-                              to = c("\\multicolumn{1}{l|}{PSO}", "\\multicolumn{1}{l|}{BBPSO-MC}",
-                                     "\\multicolumn{1}{l|}{BBPSOxp-MC}"))
-print(xtable(tabout), include.rownames=FALSE, sanitize.text.function=identity)
+  ofk <- subset(psosumorder, Obj == k)[,-1]
+  ## remove large values from table
+  bigidx <- which(ofk$Mean > 10000)
+  ofk$Mean[bigidx] <- NA
+  ofk$SD[bigidx] <- NA
+  ## pull out each type of nbhd and combine
+  ofk1 <- subset(ofk, Nbhd == "Global")[,-2]
+  ofk2 <- subset(ofk, Nbhd == "SS3")[,-2]
+  ofk3 <- subset(ofk, Nbhd == "SS1")[,-2]
+  ofkout <- cbind(ofk1, ofk2[,-1], ofk3[,-1])
+  ## round
+  ofkout2 <- ofkout
+  ofkout2[,-1] <- round(signif(ofkout2[,-1], 5), 4)
+  ## properly label \widehat{K}'s that are large
+  ofkout2[ofkout2 == "Inf"] <- "$> 1000$"
+  print(xtable(ofkout2, digits = c(0,0,rep(c(2,2,2,2),3)), align = "ll|rrrr|rrrr|rrrr",
+               caption = paste("Simulation results for OF", k, ". See text for description", sep = ""),
+               label = paste("tab:psosim", k, sep="")),
+        include.rownames=FALSE, sanitize.text.function=identity, hline.after=c(-1,seq(0, 24, 4)),
+        size = "\\scriptsize")
 }
 
-library(ggplot2)
 
-atplotout <- subset(psoout, (obj %in% c(1, 8) & nbhd == "ring-1" & type == "AT-PSO-0.5" &
-                             time > 0 & rep == 45))
-atplotout <- rbind(atplotout, data.frame(obj = 1, logpost = 0, argnorm = 0, time = 1:niter, type = "DI-PSO-200-1", nbhd = "ring-1", rep = 1, inertias = 1/(1 + ((1:niter + 1)/200)^1)))
-atplotout$obj[atplotout$obj == 8] <- 6
-atplotout$Algorithm <- paste(atplotout$type, ", Obj = ", atplotout$obj, sep = "")
-atplotout$Algorithm <- mapvalues(atplotout$Algorithm, "DI-PSO-200-1, Obj = 1", "DI-PSO-200-1")
+
+
+rr <- 2
+algidstr <- "PSO-2-CF-AT2"
+atplotout <- subset(psoout, (obj %in% c(1, 6) & nbhd == 3 & algid == algidstr &
+                             time > 0 & rep == rr))[,c(1,4,5,12)]
+atplotout <- rbind(atplotout, data.frame(obj = 1, time = 1:niter, algid = "DI-PSO", inertias = 1/(1 + ((1:niter + 1)/200)^1)))
+atplotout$Algorithm <- mapvalues(atplotout$algid, algidstr, "AT5-PSO2-CF")
+atplotout$Algorithm <- paste(atplotout$Algorithm, ", Obj = ", atplotout$obj, sep = "")
+atplotout$Algorithm <- mapvalues(atplotout$Algorithm, "DI-PSO, Obj = 1", "DI-PSO")
 atplotout$Algorithm <- factor(atplotout$Algorithm, unique(atplotout$Algorithm)[c(3,1,2)])
 inertiaplot <- qplot(time, inertias, data = atplotout, geom = "line", linetype = Algorithm) +
   xlab("iteration") + ylab("inertia")
 
+algidstr2 <- "BBPSO-1-CF-AT1"
+atplotout2 <- subset(psoout, (obj %in% 1:6 & nbhd == 3 & algid == algidstr2 &
+                             time > 0 & rep == rr))[,c(1,4,5,12)]
+atplotout2$Obj <- paste("OF", atplotout2$obj, sep="")
+scaleplot <- qplot(time, inertias, data = atplotout2, geom = "line", linetype = Obj) +
+  xlab("iteration") + ylab("scale")
+scaleplot
 
-ht <- 3
-wd <- 6
+ht <- 4
+wd <- 8
 ggsave("inertiaplot.png", inertiaplot, width = wd, height = ht)
 
-
-
-
-
-
-
-
-
-
-
-#################### old under hear ########################
-
-
-
-
-sumlastmod <- sumlast
-sumlastmod <- sumlastmod[!(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==1),]
-sumlastmod$q05[(sumlastmod$q05 < sumlastmod$q25[(sumlastmod$obj == 1 & sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Ring-3")] & sumlastmod$obj == 1)] <- sumlastmod$q25[(sumlastmod$obj == 1 & sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Ring-3")]
-sumlastmod <- sumlastmod[!(sumlastmod$Algorithm == "PSO" & sumlastmod$obj == 3) & !(sumlastmod$Algorithm ==  "AT-BBPSO-MC-1" & sumlastmod$obj == 3),]
-
-
-
-
-sumlastmod$q05[(sumlastmod$Algorithm == "BBPSOxp-MC" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==1)] <- sumlastmod$q25[(sumlastmod$Algorithm == "BBPSOxp-MC" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==1)]
-sumlastmod$q05[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==1)] <- sumlastmod$q25[(sumlastmod$Algorithm == "BBPSOxp-MC" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==1)]
-sumlastmod$q05[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)] <- sumlastmod$q25[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)]
-sumlastmod$q05[(sumlastmod$Algorithm == "BBPSOxp-MC" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)] <- sumlastmod$q25[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)]
-sumlastmod$q05[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)] <- sumlastmod$q25[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)]
-sumlastmod$q05[(sumlastmod$Algorithm == "AT-BBPSO-MC-1" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)] <- sumlastmod$q25[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==2)]
-sumlastmod$q05[(sumlastmod$Algorithm == "AT-BBPSO-MC-1" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==3)] <- sumlastmod$q25[(sumlastmod$Algorithm == "AT-BBPSO-MC-1" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==3)]
-sumlastmod$q05[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==4)] <- sumlastmod$q25[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==4)]
-sumlastmod$q95[(sumlastmod$Algorithm == "AT-BBPSO-MC-1" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==6)] <- -18
-sumlastmod$q95[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-1" & sumlastmod$Nbhd == "Global" & sumlastmod$obj==6)] <- -18
-sumlastmod$q05[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Ring-1" & sumlastmod$obj==1)] <- -100
-sumlastmod$q05[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Ring-1" & sumlastmod$obj==3)] <- sumlastmod$q25[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Ring-1" & sumlastmod$obj==3)]
-sumlastmod$q05[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Ring-3" & sumlastmod$obj==3)] <- -21000
-sumlastmod$q05[(sumlastmod$Algorithm == "PSO" & sumlastmod$Nbhd == "Ring-3" & sumlastmod$obj==1)] <- -400
-sumlastmod$q05[(sumlastmod$Algorithm == "BBPSOxp-MC" & sumlastmod$Nbhd == "Ring-3" & sumlastmod$obj==1)] <- -400
-sumlastmod$q05[(sumlastmod$Algorithm == "AT-BBPSOxp-MC-2" & sumlastmod$Nbhd == "Ring-3" & sumlastmod$obj==1)] <- -400
-
-library(ggplot2)
-
-maxplot1 <- ggplot(subset(sumlastmod, obj == 1)) +
-  geom_boxplot(aes(x=Algorithm, lower = q25, upper = q75, middle = Median, ymin = q05,
-                   ymax = q95), stat="identity") +
-  facet_wrap(Nbhd~obj, ncol = 1,
-             labeller = label_bquote(paste(.(Nbhd), ", ", f[.(obj)]))) +
-  theme(axis.text = element_text(angle = 45, hjust = 1),
-                strip.text = element_text(size = 11)) +
-  scale_x_discrete(drop=FALSE)
-maxplot2 <- ggplot(subset(sumlastmod, obj == 2)) +
-  geom_boxplot(aes(x=Algorithm, lower = q25, upper = q75, middle = Median, ymin = q05,
-                   ymax = q95), stat="identity") +
-  facet_wrap(Nbhd~obj, ncol = 1,
-             labeller = label_bquote(paste(.(Nbhd), ", ", f[.(obj)]))) +
-  theme(axis.text = element_text(angle = 45, hjust = 1),
-                strip.text = element_text(size = 11)) +
-  scale_x_discrete(drop=FALSE)
-maxplot3 <- ggplot(subset(sumlastmod, obj == 3)) +
-  geom_boxplot(aes(x=Algorithm, lower = q25, upper = q75, middle = Median, ymin = q05,
-                   ymax = q95), stat="identity") +
-  facet_wrap(Nbhd~obj, ncol = 1,
-             labeller = label_bquote(paste(.(Nbhd), ", ", f[.(obj)]))) +
-  theme(axis.text = element_text(angle = 45, hjust = 1),
-                strip.text = element_text(size = 11)) +
-  scale_x_discrete(drop=FALSE)
-maxplot4 <- ggplot(subset(sumlastmod, obj == 4)) +
-  geom_boxplot(aes(x=Algorithm, lower = q25, upper = q75, middle = Median, ymin = q05,
-                   ymax = q95), stat="identity") +
-  facet_wrap(Nbhd~obj, ncol = 1,
-             labeller = label_bquote(paste(.(Nbhd), ", ", f[.(obj)]))) +
-  theme(axis.text = element_text(angle = 45, hjust = 1),
-                strip.text = element_text(size = 11)) +
-  scale_x_discrete(drop=FALSE)
-maxplot5 <- ggplot(subset(sumlastmod, obj == 5)) +
-  geom_boxplot(aes(x=Algorithm, lower = q25, upper = q75, middle = Median, ymin = q05,
-                   ymax = q95), stat="identity") +
-  facet_wrap(Nbhd~obj, ncol = 1,
-             labeller = label_bquote(paste(.(Nbhd), ", ", f[.(obj)]))) +
-  theme(axis.text = element_text(angle = 45, hjust = 1),
-                strip.text = element_text(size = 11)) +
-  scale_x_discrete(drop=FALSE)
-maxplot6 <- ggplot(subset(sumlastmod, obj == 6)) +
-  geom_boxplot(aes(x=Algorithm, lower = q25, upper = q75, middle = Median, ymin = q05,
-                   ymax = q95), stat="identity") +
-  facet_wrap(Nbhd~obj, ncol = 1,
-             labeller = label_bquote(paste(.(Nbhd), ", ", f[.(obj)]))) +
-  theme(axis.text = element_text(angle = 45, hjust = 1),
-                strip.text = element_text(size = 11)) +
-  scale_x_discrete(drop=FALSE)
-
-
-options(scipen = -2)
-ht <- 6
-wd <- 3
-ggsave("maxplot11.png", maxplot1, width = wd, height = ht)
-ggsave("maxplot12.png", maxplot2, width = wd, height = ht)
-ggsave("maxplot13.png", maxplot3, width = wd, height = ht)
-ggsave("maxplot14.png", maxplot4, width = wd, height = ht)
-ggsave("maxplot15.png", maxplot5, width = wd, height = ht)
-ggsave("maxplot16.png", maxplot6, width = wd, height = ht)
-
-
-
-options(scipen = -1)
-ggsave("argmaxplot21.png", argmaxplot1, width = 12, height = 7)
-ggsave("argmaxplot22.png", argmaxplot2, width = 12, height = 7)
+ht <- 4
+wd <- 8
+ggsave("scaleplot.png", scaleplot, width = wd, height = ht)
