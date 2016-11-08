e6fd57b98108c0b17061d787042786c105c0f991
Matt <themattsimpson@gmail.com>
2016-11-08 12:23:08 -0600

    trying to get the mem-per-cpu thing to work

--- code/kriging/psoruns.sh
+++ code/kriging/psoruns.sh
@@ -6,7 +6,7 @@
 #SBATCH -J psorunskriging
 #SBATCH -o psoruns.o
 #SBATCH -e psoruns.e
-#SBATCH -mem-per-cpu=3G
+#SBATCH --mem-per-cpu=3G
 #SBATCH -t 7-00:00
 #SBATCH -n 28  # cores
 #SBATCH -N 1  # nodes
